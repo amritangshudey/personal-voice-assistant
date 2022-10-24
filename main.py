22496e477385f792d7a2d4b81e75205101a32c77
amritangshudey <76175119+amritangshudey@users.noreply.github.com>
2022-10-24 20:54:50 +0530

    files uploaded

---
+++ main.py
@@ -0,0 +1,195 @@
+import pyttsx3
+import requests
+import speech_recognition
+import os
+from bs4 import BeautifulSoup
+import datetime
+import pyautogui
+from pygame import mixer
+import random
+from plyer import notification
+import webbrowser
+import wolframalpha 
+import ctypes
+ 
+
+
+engine = pyttsx3.init("sapi5")
+voices = engine.getProperty("voices")
+engine.setProperty("voice", voices[0].id)
+rate = engine.setProperty("rate",170)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def takeCommand():
+    r = speech_recognition.Recognizer()
+    with speech_recognition.Microphone() as source:
+        r.adjust_for_ambient_noise(source,duration=0.2)
+        print("Listening.....")
+        r.pause_threshold = 1
+        r.energy_threshold = 300
+        audio = r.listen(source,0,4)
+
+    try:
+        print("Understanding..")
+        query  = r.recognize_google(audio,language='en-in')
+        print(f"You Said: {query}\n")
+    except Exception as e:
+        print("Say that again")
+        return "None"
+    return query
+
+
+
+
+
+
+if __name__ == "__main__":
+    while True:
+        query = takeCommand().lower()
+        if "power on" in query:
+            from GreetMe import greetMe
+            greetMe()
+
+            while True:
+                query = takeCommand().lower()
+                if "sleep" in query:
+                    speak("Ok sir , You can me call anytime")
+                    break
+                elif "hello" in query:
+                    speak("Hello sir, how are you ?")
+                elif "i am fine" in query:
+                    speak("that's great, sir")
+                elif "how are you" in query:
+                    speak("Perfect, sir")
+                elif "thank you" in query:
+                    speak("you are welcome, sir")
+                elif "google" in query:
+                    from SearchNow import searchGoogle
+                    searchGoogle(query)
+                elif "youtube" in query:
+                    from SearchNow import searchYoutube
+                    searchYoutube(query)
+                elif "wikipedia" in query:
+                    from SearchNow import searchWikipedia
+                    searchWikipedia(query)
+                elif "temperature" in query:
+                    search = "temperature in kr puram"
+                    url = f"https://www.google.com/search?q={search}"
+                    r  = requests.get(url)
+                    data = BeautifulSoup(r.text,"html.parser")
+                    temp = data.find("div", class_ = "BNeawe").text
+                    speak(f"current{search} is {temp}")
+                    print(f"current{search} is {temp}")
+                elif "the time" in query:
+                    strTime = datetime.datetime.now().strftime("%H:%M")    
+                    speak(f"Sir, the time is {strTime}")
+                elif "power off" in query:
+                    speak("Going to sleep,sir")
+
+
+                    exit()
+                elif "open" in query:
+                    from Dictapp import openappweb
+                    openappweb(query)
+                elif "close" in query:
+                    from Dictapp import closeappweb
+                    closeappweb(query) 
+
+                elif "pause" in query:
+                    pyautogui.press("k")
+                    speak("video paused")
+                elif "play" in query:
+                    pyautogui.press("k")
+                    speak("video played")
+                elif "mute" in query:
+                    pyautogui.press("m")
+                    speak("video muted")
+
+                elif "volume up" in query:
+                    from keyboard import volumeup
+                    speak("Turning volume up,sir")
+                    volumeup()
+                elif "volume down" in query:
+                    from keyboard import volumedown
+                    speak("Turning volume down, sir")
+                    volumedown()
+                elif "remember that" in query:
+                    rememberMessage = query.replace("remember that","")
+                    rememberMessage = query.replace("android","")
+                    speak("You told me to "+rememberMessage)
+                    remember = open("Remember.txt","w")
+                    remember.write(rememberMessage)
+                    remember.close()
+                elif "what do you remember" in query:
+                    remember = open("Remember.txt","r")
+                    speak("You told me tok" + remember.read())
+                elif "calculate" in query:
+                    app_id = "AA4UPR-RHHTV4KEQ6"
+                    client = wolframalpha.Client(app_id)
+                    indx = query.lower().split().index('calculate')
+                    query = query.split()[indx + 1:]
+                    res = client.query(' '.join(query))
+                    answer = next(res.results).text
+                    print("The answer is " + answer)
+                    speak("The answer is " + answer)
+                elif "schedule my day" in query:
+                    tasks = [] #Empty list 
+                    speak("Do you want to clear old tasks (Plz speak YES or NO)")
+                    query = takeCommand().lower()
+                    if "yes" in query:
+                        file = open("tasks.txt","w")
+                        file.write(f"")
+                        file.close()
+                        no_tasks = int(input("Enter the no. of tasks :- "))
+                        i = 0
+                        for i in range(no_tasks):
+                            
+                            tasks.append(input("Enter the task :- "))
+                            file = open("tasks.txt","a")
+                            file.write(f"{i}. {tasks[i]}\n")
+                            file.close()
+                    elif "no" in query:
+                        i = 0
+                        no_tasks = int(input("Enter the no. of tasks :- "))
+                        for i in range(no_tasks):
+                            tasks.append(input("Enter the task :- "))
+                            file = open("tasks.txt","a")
+                            file.write(f"{i}. {tasks[i]}\n")
+                            file.close()
+                elif "show my schedule" in query:
+                    file = open("tasks.txt","r")
+                    content = file.read()
+                    file.close()
+                    mixer.init()
+                    mixer.music.load("notification.mp3")
+                    mixer.music.play()
+                    notification.notify(
+                        title = "My schedule :-",
+                        message = content,
+                        timeout = 15
+                        )
+                elif "launch" in query:   
+                    query = query.replace("launch","")
+                    query = query.replace("android","")
+                    pyautogui.press("super")
+                    pyautogui.typewrite(query)
+                    pyautogui.sleep(2)
+                    pyautogui.press("enter") 
+                elif "game on" in query:
+                    from game import game_play
+                    game_play()
+                elif "click my photo" in query:
+                    pyautogui.press("super")
+                    pyautogui.typewrite("camera")
+                    pyautogui.press("enter")
+                    pyautogui.sleep(2)
+                    speak("SMILE")
+                    pyautogui.press("enter")
+                elif 'lock window' in query:
+                    speak("locking the device")
+                    ctypes.windll.user32.LockWorkStation()
+               
